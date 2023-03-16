223581b01277f6cb7a8ae8b76c7a958759b6a744
alejandro lobo <alejandro.lobo_97@hotmail.com>
2023-03-16 16:55:33 -0300

    css

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,43 +1,45 @@
-// const ITEMS= 'items';
+const ITEMS= 'items';
 
-// function getInicio() {
-//     $.get("data.json", function(respuesta, estado){
-//         if(estado === "success"){
-//             jsonReader(respuesta);
-//         }
-//     });
-// }
+function getInicio() {
+    $.get("Scripts/data.json", function(respuesta, estado){
+        if(estado === "success"){
+            jsonReader(respuesta);
+        }
+    });
+}
+getInicio();
 
-// this.lista= [];
-// this.listaItems= [];
+this.lista= [];
+this.listaItems= [];
 
-// function jsonReader(){
-//     i=0;
-//     for(const dato of respuesta){
-//         i++;
-//         const item = {
-//             "id": `${i}`,
-//             "mayus": `${dato.mayus}`,
-//             "nombre": `${dato.nombre}`,
-//             "precio": `${dato.precio}`,
-//             "stock": `${dato.stock}`,
-//             "imagen": `${dato.imagen}`,
-//             "oferta": `${dato.oferta}`
-//         };
-//         lista.push(item);
-//     }
-// }
+function jsonReader(respuesta){
+    i=0;
+    for(const dato of respuesta){
+        i++;
+        const item = {
+            "id": `${i}`,
+            "mayus": `${dato.mayus}`,
+            "nombre": `${dato.nombre}`,
+            "precio": `${dato.precio}`,
+            "stock": `${dato.stock}`,
+            "imagen": `${dato.imagen}`,
+            "oferta": `${dato.oferta}`
+        };
+        lista.push(item);
+    }
+    initLista();
+}
 
-// function initLista() {
-//     tempLista= json.parse(localStorage.getItem(ITEMS));
-//     if(tempLista == null || tempLista == undefined){
-//         localStorage.setItem(ITEMS, JSON.stringify(lista));
-//     }else{
-//         lista= tempLista;
-//     }
-//     lista.map((item) => {
-//         i= product.id;
-//         //AGREGAR FUNCION PARA AGREGAR HTML
-//         console.log(item)
-//     })
-// }
+function initLista() {
+    tempLista= JSON.parse(localStorage.getItem(ITEMS));
+    if(tempLista == null || tempLista == undefined){
+        localStorage.setItem(ITEMS, JSON.stringify(lista));
+    }else{
+        lista= tempLista;
+    }
+    lista.map((item) => {
+        i= item.id;
+        //AGREGAR FUNCION PARA AGREGAR HTML
+        console.log(item)
+    })
+}
